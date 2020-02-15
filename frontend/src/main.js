b00a8503abf5c7b11e81b6bd780ce82d7f0dade5
Denis Pozdnyakov <oooeeee@gmail.com>
2020-02-16 01:44:55 +0300

    Using bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLock, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// app.js
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+// icons one-by-one
+library.add(faLock)
+library.add(faCheck)
+ 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
